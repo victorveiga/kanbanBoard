6b498c7579aa20022845b7a8affafdd35e148f96
victorveiga <victorl.veigapro@gmail.com>
2020-03-15 10:56:16 -0300

    ajustado a resposta de movimentação dos cards

--- public/js/script.js
+++ public/js/script.js
@@ -11,28 +11,6 @@ function formularioDefault() {
 function AbreCard(id) {
 
     let obj = null
-    /*let http = new XMLHttpRequest()
-    http.open('GET', 'getChamado/'+id)
-    http.onreadystatechange = () => {
-        if(http.readyState == XMLHttpRequest.DONE && http.status == 200){
-            obj = JSON.parse(http.response)[0]
-            $('#recipient_name').val(obj.titulo)
-            $('#message_text'  ).val(obj.descricao)
-            $('#tipo_protocolo').val(obj.tipo_protocolo)
-            $('#select_status' ).val(obj.status)
-            
-            $('#ModalLabel').text(`Protocolo Nº ${id}`)
-
-            $('#form_protocolo').removeAttr('action')
-            $('#form_protocolo').removeAttr('method')
-
-            $('#form_protocolo').attr('action', '/atualizarProtocolo/'+id)
-            $('#form_protocolo').attr('method','POST'  )
-
-            $('#formProtocolo').modal('show')
-        }
-    }
-    http.send() */
     getChamado(id).then((obj)=>{
         $('#recipient_name').val(obj.titulo)
         $('#message_text'  ).val(obj.descricao)
@@ -127,13 +105,5 @@ let xColunas = Array(
 let socket = io()
 // Comunicação via socket
 socket.on('cards atualizados', (data) => {
-
-    let {id,id_coluna} = data
-
-    getChamado(id).then((obj)=>{
-        let cardOrigem = $('#card_'+obj.id)
-        let colunaDestino = $('#'+id_coluna)
-        colunaDestino.append(cardOrigem)
-    })
-
+    window.location.href = '/'
 })
